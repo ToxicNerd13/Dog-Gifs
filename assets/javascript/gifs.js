ef486a60283b0a3423f80de13aaf468b936007f4
ToxicNerd <k.lehenbauer13@gmail.com>
2019-09-26 23:29:53 -0700

    Got click event working on new buttons, made it look prettier

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
     function makeButton(breed) {
         let button = $("<button>").text(breed);
         button.attr("data-breed", breed);
+        button.attr("class", "btn btn-outline-dark m-1 dog-button");
         $("#dog-buttons").append(button);
     };
 
@@ -15,9 +16,21 @@ $(document).ready(function () {
         makeButton(topics[i]);
     };
 
+    // Add new button based on user input
+    $("#submit-button").on("click", function (event) {
+        event.preventDefault();
+
+        let userInput = $("#add-button").val().trim();
+        topics.push(userInput);
+        makeButton(userInput);
+        $(".form-control").val("");
+    });
+
     // When button is clicked, make API call to get gifs
-    $("button").on("click", function () {
+    $(document).on("click", ".dog-button", function () {
+        // Clear whatever gifs were displayed from last category
         $("#gif-container").empty();
+
         let dogBreed = $(this).attr("data-breed");
         let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=PXPeoxwY4DfSuEuFjEYoP6c44E0Ny00Z&q=${dogBreed}&limit=10&offset=0&lang=en`;
 
@@ -25,23 +38,42 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET"
         })
-        .then(function(response) {
-            let gifs = response.data;
-            // Only return gifs that are not R-rated
-            for (let i = 0; i < gifs.length; i++) {
-                if (gifs[i].rating !== "r") {
-                    let gifDiv = $("<div>");
-                    // Change rating to uppercase
-                    let rating = gifs[i].rating.toUpperCase();
-                    let p = $("<p>").text(`Rating: ${rating}`);
-                    // Display still image of gif
-                    let dogGif = $("<img>").attr("src", gifs[i].images.fixed_height_still.url);
-                    gifDiv.append(dogGif);
-                    gifDiv.append(p);
-                    $("#gif-container").append(gifDiv);
-                } 
-            }
-        })
+            .then(function (response) {
+                let gifs = response.data;
+                // Only return gifs that are not R-rated
+                for (let i = 0; i < gifs.length; i++) {
+                    if (gifs[i].rating !== "r") {
+                        let gifDiv = $("<div>");
+                        // Change rating to uppercase
+                        let rating = gifs[i].rating.toUpperCase();
+                        let p = $("<p>").text(`Rating: ${rating}`);
+                        // Display still image of gif
+                        let dogGif = $("<img>").attr({
+                            "src": gifs[i].images.fixed_height_still.url,
+                            "data-still": gifs[i].images.fixed_height_still.url,
+                            "data-animate": gifs[i].images.fixed_height.url,
+                            "data-state": "still",
+                            "class": "gif"
+                        });
+                        // Add gif and rating to page display
+                        gifDiv.append(dogGif);
+                        gifDiv.append(p);
+                        $("#gif-container").append(gifDiv);
+                    }
+                };
+                $(".gif").on("click", function () {
+                    let state = $(this).attr("data-state");
+
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate")
+                    }
+                    else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
+            })
     });
 
 })
